30ad0305688bff99c54f3f07ded4ef3150df39f8
samantaavijit <samantaavijit.2000@gmail.com>
2021-03-04 16:56:02 +0530

    leetcode RemoveElement

--- src/com/avijitsamanta/leetcode/array/RemoveElement.java
+++ src/com/avijitsamanta/leetcode/array/RemoveElement.java
@@ -1,5 +1,12 @@
 package com.avijitsamanta.leetcode.array;
 
+
+/***
+ * @author Avijit Samanta
+ *
+ * Question link
+ * https://leetcode.com/problems/remove-element/
+ */
 public class RemoveElement {
     private static int removeElement(int[] nums, int val) {
         int c = 0;
