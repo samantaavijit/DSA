688f7197bb805bd5273e6ffcc7ba735ea6ba6cc1
samantaavijit <samantaavijit.2000@gmail.com>
2021-03-05 21:16:19 +0530

    gfg BetweenTwoSets

---
+++ src/com/avijitsamanta/gfg/array/BetweenTwoSets.java
@@ -0,0 +1,44 @@
+package com.avijitsamanta.gfg.array;
+
+/***
+ * @author Avijit Samanta
+ *
+ *
+ * Question link
+ * https://www.hackerrank.com/challenges/between-two-sets/problem
+ */
+public class BetweenTwoSets {
+    private static int getTotalX(int[] a, int[] b) {
+        int n = a.length;
+        int x = a[n - 1], count = 0;
+        boolean flag;
+
+        while (x <= b[0]) {
+            flag = false;
+            for (int k : a) {
+                if (x % k != 0) {
+                    flag = true;
+                    break;
+                }
+            }
+            if (!flag) {
+                for (int j : b) {
+                    if (j % x != 0) {
+                        flag = true;
+                        break;
+                    }
+                }
+            }
+
+            if (!flag) count++;
+            x++;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {2, 4};
+        int[] b = {16, 32, 96};
+        System.out.println(getTotalX(a, b));
+    }
+}
