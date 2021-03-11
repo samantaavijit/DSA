d196c4c4efbdb42cfeb06a8f2eabee7d9ae5570b
samantaavijit <samantaavijit.2000@gmail.com>
2021-03-12 01:02:42 +0530

    hackerrank FindDigits

---
+++ src/com/avijitsamanta/hackertrank/array/FindDigits.java
@@ -0,0 +1,23 @@
+package com.avijitsamanta.hackertrank.array;
+
+/**
+ * @author Avijit Samanta
+ * <p>
+ * Question link
+ * https://www.hackerrank.com/challenges/find-digits/problem
+ */
+public class FindDigits {
+    private static int findDigits(int n) {
+        int c = 0, a = n, d;
+        while (a > 0) {
+            d = a % 10;
+            if (d != 0 && n % d == 0) c++;
+            a /= 10;
+        }
+        return c;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(findDigits(1012));
+    }
+}
