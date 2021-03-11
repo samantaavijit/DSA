ed44287d34c7902eb966c2de27457856a6a2a57a
samantaavijit <samantaavijit.2000@gmail.com>
2021-03-11 21:07:56 +0530

    hackerrank PickingNumbers

---
+++ src/com/avijitsamanta/hackertrank/array/PickingNumbers.java
@@ -0,0 +1,31 @@
+package com.avijitsamanta.hackertrank.array;
+
+import java.util.Arrays;
+
+/**
+ * @author Avijit Samanta
+ * <p>
+ * Question link
+ * https://www.hackerrank.com/challenges/picking-numbers/problem
+ */
+public class PickingNumbers {
+    public static int pickingNumbers(int[] a) {
+        Arrays.sort(a);
+        int c, max = -1;
+        for (int i = 0; i < a.length; i++) {
+            c = 1;
+            for (int j = i + 1; j < a.length; j++) {
+                if (a[i] == a[j] || a[i] == a[j] + 1 || a[i] == a[j] - 1)
+                    c++;
+                else break;
+            }
+            max = Math.max(max, c);
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 1, 1, 2, 3, 4};
+        System.out.println(pickingNumbers(arr));
+    }
+}
