e068c3f4fdc52baf05a0deb7b00ca7fc1dd36ae1
samantaavijit <samantaavijit.2000@gmail.com>
2021-03-11 21:33:58 +0530

    hackerrank DesignerPDFViewer

---
+++ src/com/avijitsamanta/hackertrank/array/DesignerPDFViewer.java
@@ -0,0 +1,21 @@
+package com.avijitsamanta.hackertrank.array;
+
+/**
+ * @author Avijit Samanta
+ * <p>
+ * Question link
+ * https://www.hackerrank.com/challenges/designer-pdf-viewer/problem
+ */
+public class DesignerPDFViewer {
+    private static int designerPdfViewer(int[] h, String word) {
+        int height = -1;
+        for (char c : word.toCharArray())
+            height = Math.max(height, h[c - 65 - 32]);
+        return height * word.length();
+    }
+
+    public static void main(String[] args) {
+        int[] h = {1, 3, 1, 3, 1, 4, 1, 3, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 7};
+        System.out.println(designerPdfViewer(h, "zaba"));
+    }
+}
